18af4d6a4432afd1278801de61b5125789b31fb3
Kubapatoka <bukaj.k@wp.pl>
2022-10-14 07:41:50 +0200

    last commit

--- 1/so21_lista_1/fileref.c
+++ 1/so21_lista_1/fileref.c
@@ -7,6 +7,19 @@ static char buf[256];
 #define LINE3 78
 
 static void do_read(int fd) {
+	pid_t pid;
+	pid = Fork();
+
+	if(pid == 0) /*child*/
+	{
+		Read(fd, buf, 16);
+		printf("%d : %ld \n", pid, Lseek(fd, 0, SEEK_CUR));
+	}
+	else /*parent*/
+	{
+		Read(fd, buf, 15);
+		printf("%d : %ld \n", pid, Lseek(fd, 0, SEEK_CUR));
+	}
   /* TODO: Spawn a child. Read from the file descriptor in both parent and
    * child. Check how file cursor value has changed in both processes. */
 
@@ -14,6 +27,22 @@ static void do_read(int fd) {
 }
 
 static void do_close(int fd) {
+	int child_status;
+	pid_t pid;
+	pid = Fork();
+	if(pid == 0)
+	{
+		Close(fd);
+		printf("%d: closing file %d\n", pid, fd);
+	}
+	else
+	{
+		Waitpid(pid, &child_status, 0);
+		printf("%d:Child status: %d\n",pid, child_status);
+		printf("%d: Cursor position: %ld\n", pid, Lseek(fd,0,SEEK_CUR));
+	       	Read(fd, buf, 14);
+		printf("%d:Reading from file %d: %s\n", pid, fd, buf);	
+	}
   /* TODO: In the child close file descriptor, in the parent wait for child to
    * die and check if the file descriptor is still accessible. */
 
